841326f722462c1dffbee1aeea955474b37289f4
Dham-AI <henriquediogo501@gmail.com>
2026-04-20 01:01:23 -0300

    Ficha pokemons completa, falta imgens tipo e evoluções

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const filtros = document.querySelectorAll('.filtro-item');
 const pokemons = document.querySelectorAll('.pokemon');
+const fichaModal = document.querySelector('#ficha-modal');
+const fecharFicha = document.querySelector('.ficha-fechar');
 
 filtros.forEach(botao => {
     botao.addEventListener('click', () => {
@@ -15,4 +17,34 @@ filtros.forEach(botao => {
             }
         });
     });
+});
+
+pokemons.forEach(card => {
+    card.addEventListener('click', () => {
+        // Preenche textos simples
+        document.querySelector('#ficha-titulo').textContent = card.dataset.nome;
+        document.querySelector('#ficha-altura').textContent = card.dataset.altura;
+        document.querySelector('#ficha-peso').textContent = card.dataset.peso;
+        document.querySelector('#ficha-categoria').textContent = card.dataset.categoria;
+        document.querySelector('#ficha-habilidades').textContent = card.dataset.habilidades;
+        document.querySelector('#ficha-tipo').src = card.dataset.tipo_img;
+        document.querySelector('#ficha-fraqueza').src = card.dataset.fraqueza;
+
+        document.querySelector('#evolu1').src = card.dataset.evolu1;
+        document.querySelector('#evolu2').src = card.dataset.evolu2;
+        document.querySelector('#evolu3').src = card.dataset.evolu3;
+
+        fichaModal.classList.remove('ficha--hidden');
+    });
+});
+
+fecharFicha.addEventListener('click', () => {
+    fichaModal.classList.add('ficha--hidden');
+});
+
+// Fechar ao clicar no fundo escuro
+fichaModal.addEventListener('click', (event) => {
+    if (event.target === fichaModal) {
+        fichaModal.classList.add('ficha--hidden');
+    }
 });
